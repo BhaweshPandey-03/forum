a9558fc7a1aeceb21d8af5c70d72f6126f723dd9
BhaweshPandey-03 <bhaweshpandey0310@gmail.com>
2024-05-02 20:15:54 +0530

    done

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+require('dotenv').config();
+const { connection } = require("./config/db");
+const { userRouter } = require("./routes/user.routes");
+const { postRoute } = require("./routes/post.routes");
+const cors = require("cors");
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/api",userRouter)
+app.use("/api",postRoute)
+
+app.listen(process.env.PORT, async () => {
+    try {
+        await connection;
+        console.log('Database is Connected');
+        console.log(`Server is Running on Port ${process.env.PORT}`);
+    }
+    catch (error) {
+        console.log(error);
+    }
+})
